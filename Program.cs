d4cfb060f01742fcad24d7b063d8efcf82b64e8b
fran-end <francoespejo743@gmail.com>
2026-06-20 19:05:23 -0300

    funciona la request http

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.Net;
 using System.Net.Sockets;
 using System.Threading.Tasks;
+using System.Text;
 
 ConfigReader config;
 try
@@ -41,7 +42,7 @@ finally
     socketEscucha.Close();
 }
 
-// Como estamos en top-level statements, los métodos se declaran al final del archivo
+
 void ProcesarCliente(Socket clienteSocket, ConfigReader config)
 {
     try
@@ -52,9 +53,38 @@ void ProcesarCliente(Socket clienteSocket, ConfigReader config)
         Logger.Registrar(clienteEndpoint.Address.ToString(), "GET", "/ (sin parsear aún)");
 
         using (NetworkStream stream = new NetworkStream(clienteSocket))
-        {
-            // PASO SIGUIENTE: leer bytes del stream y parsear el request HTTP
-        }
+{
+    byte[] buffer = new byte[4096];
+    int bytesLeidos = stream.Read(buffer, 0, buffer.Length);
+
+    if (bytesLeidos == 0)
+    {
+        return;
+    }
+
+    string requestCrudo = Encoding.UTF8.GetString(buffer, 0, bytesLeidos);
+
+    Console.WriteLine("----- REQUEST RECIBIDO -----");
+    Console.WriteLine(requestCrudo);
+    Console.WriteLine("-----------------------------");
+
+    // aca juli conectas el HttpRequestParser.cs
+
+    // se calcula longitud del body
+    string body = "Hola, mundo!";
+    byte[] bodyBytes = Encoding.UTF8.GetBytes(body);
+
+    string headers = "HTTP/1.1 200 OK\r\n" +
+                      $"Content-Length: {bodyBytes.Length}\r\n" +
+                      "Content-Type: text/plain\r\n" +
+                      "Connection: close\r\n" +
+                      "\r\n";
+
+    byte[] headerBytes = Encoding.UTF8.GetBytes(headers);
+
+    stream.Write(headerBytes, 0, headerBytes.Length);
+    stream.Write(bodyBytes, 0, bodyBytes.Length);
+}
     }
     catch (Exception ex)
     {
